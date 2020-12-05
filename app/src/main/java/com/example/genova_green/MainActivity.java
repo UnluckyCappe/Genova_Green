3ed7063582d9f874c8be6367734a4e63f5b74202
Andrea <cappelli.andrea.1998@gmail.com>
2020-12-05 02:41:14 +0100

    aaa

--- app/src/main/java/com/example/genova_green/MainActivity.java
+++ app/src/main/java/com/example/genova_green/MainActivity.java
@@ -1,27 +1,58 @@
 package com.example.genova_green;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     DrawerLayout dl;
+    private static String ip="192.168.1.6";
+    private static String port="1433";
+    private static String classes="net.sourceforge.jtds.jdbc.Driver";
+    private static String database="GenovaGreen";
+    private static String username="Angelo Morro";
+    private static String password= "barcollomanonMorro";
+    private static String url="jdbc:jtds:sqlserver://"+ip+":"+port+"/"+database;
+
+    private Connection connection=null;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.INTERNET}, PackageManager.PERMISSION_GRANTED);
+
         dl=findViewById(R.id.home);
+
+        StrictMode.ThreadPolicy policy=new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+        try {
+            Class.forName(classes);
+            connection= DriverManager.getConnection(url,username,password);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
     }
     public void ClickMenu(View view){
